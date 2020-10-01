527ba8aa21edfbc74f2b0a213b74eacb97cd4c75
Makena <soniamakena02@gmail.com>
2020-10-02 00:09:08 +0300

    final changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,12 +10,23 @@ import static spark.route.HttpMethod.post;
 
 
 public class App{
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main(String[]args){
+        port(getHerokuAssignedPort());
         Sql2oUsersDao usersDao;
         Connection connection;
         Gson gson = new Gson();
-        String connectionString = "jdbc:postgresql://localhost:5432/motivation";
-        Sql2o sql2o = new Sql2o(connectionString, "makena", "barbie");
+//        String connectionString = "jdbc:postgresql://localhost:5432/motivation";
+//        Sql2o sql2o = new Sql2o(connectionString, "makena", "barbie");
+
+        String connectionString = "jdbc:postgresql://ec2-3-226-231-4.compute-1.amazonaws.com:5432/d81o3b822e58ua"; //!
+        Sql2o sql2o = new Sql2o(connectionString, "vqmvlgmcjnmuwt", "4c7c2c19e3cc50b47044037ae819eaf2eda151282b31557363ff352f62842f5f"); //!
         usersDao = new Sql2oUsersDao(sql2o);
 
         post("/user/new", "application/json", (request, response) -> {
