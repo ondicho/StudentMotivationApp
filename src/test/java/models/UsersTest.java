98657823ddea778ea3f3c43765e13e665ddc5189
Glory Sonia <67630768+Sonia0-0@users.noreply.github.com>
2020-10-01 16:27:18 +0300

    Update UsersTest.java

--- src/test/java/models/UsersTest.java
+++ src/test/java/models/UsersTest.java
@@ -1,6 +1,38 @@
 package models;
 
 import junit.framework.TestCase;
-public class UsersTest extends TestCase {
-  
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UsersTest  {
+    @Test
+    public void getCorrectName_String() {
+        Users users = new Users("Trevor", 20, 1, "trevor@mail.com","male");
+        assertEquals("Trevor", users.getName());
+    }
+
+    @Test
+    public void getCorrectAge_Int() {
+        Users users = new Users("Trevor", 20, 1, "trevor@mail.com","male");
+        assertEquals(20, users.getAge());
+    }
+
+    @Test
+    public void getCorrectEmail_String() {
+        Users users = new Users("Trevor", 20, 1, "trevor@mail.com","male");
+        assertEquals("trevor@mail.com", users.getEmail());
+    }
+
+    @Test
+    public void getCorrectGender_String() {
+        Users users = new Users("Trevor", 20, 1, "trevor@mail.com","male");
+        assertEquals("male", users.getGender());
+    }
+
+    @Test
+    public void getCorrectId_Int() {
+        Users users = new Users("Trevor", 20, 1, "trevor@mail.com","male");
+        assertEquals(1, users.getId());
+    }
+
 }
