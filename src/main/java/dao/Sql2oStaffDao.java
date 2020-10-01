73e6fd456c5e58171c17da0443a7617fda99828b
Ondicho <ondicho53@gmail.com>
2020-10-02 00:50:08 +0300

    generates equals and hashcode for user object

--- src/main/java/dao/Sql2oStaffDao.java
+++ src/main/java/dao/Sql2oStaffDao.java
@@ -1,5 +1,28 @@
 package dao;
 
-public class Sql2oStaffDao {
+import models.user;
 
+import java.util.List;
+
+public class Sql2oStaffDao implements userDao,StaffDao{
+
+    @Override
+    public void add(user user) {
+
+    }
+
+    @Override
+    public List<user> getAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
--- src/main/java/models/user.java
+++ src/main/java/models/user.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class user {
 
     private String name;
@@ -8,51 +10,90 @@ public class user {
     private String email;
     private String department;
     private int phoneNumber;
+    private String role;
 
-    public user(int id,String name, int age, String email, String department, int phoneNumber){
+    public user(int id, String name, int age, String email, String department, int phoneNumber, String role) {
         this.name = name;
         this.age = age;
         this.id = id;
         this.email = email;
         this.department = department;
         this.phoneNumber = phoneNumber;
+        this.role = role;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getAge(){
-        return age;
-    }
-    public int getId(){
-        return id;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getDepartment(){
-        return department;
-    }
-    public int getPhoneNumber(){
-        return phoneNumber;
-    }
-    public void setName(){
+    public void setName(String name) {
         this.name = name;
     }
-    public void setAge(){
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
-    public void setId(int id){
-        this.id = this.id;
+
+    public int getId() {
+        return id;
     }
-    public void setEmail(){
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
-    public void setDepartment(){
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
         this.department = department;
     }
-    public void setPhoneNumber(){
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        user user = (user) o;
+        return age == user.age &&
+                id == user.id &&
+                phoneNumber == user.phoneNumber &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(department, user.department) &&
+                Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, id, email, department, phoneNumber, role);
     }
 }
